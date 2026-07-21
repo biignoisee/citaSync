e36f729a672506fac2895564ead75579020354e5
Omar Gabriel <omareegab@gmail.com>
2026-07-21 09:58:49 -0500

    ui guideline pt1 || welcome y conf server

--- citaSync/resources/views/pages/auth/register.blade.php
+++ citaSync/resources/views/pages/auth/register.blade.php
@@ -1,6 +1,6 @@
 <x-layouts::auth :title="__('Register')">
     <div class="flex flex-col gap-6">
-        <x-auth-header :title="__('Create an account')" :description="__('Enter your details below to create your account')" />
+        <x-auth-header :title="__('Crear una cuenta')" :description="__('Ingresa tus datos a continuación para crear tu cuenta')" />
 
         <!-- Session Status -->
         <x-auth-session-status class="text-center" :status="session('status')" />
@@ -8,62 +8,35 @@
         <form method="POST" action="{{ route('register.store') }}" class="flex flex-col gap-6">
             @csrf
             <!-- Name -->
-            <flux:input
-                name="name"
-                :label="__('Name')"
-                :value="old('name')"
-                type="text"
-                required
-                autofocus
-                autocomplete="name"
-                :placeholder="__('Full name')"
-            />
+            <flux:input name="name" :label="__('Nombre')" :value="old('name')" type="text" required autofocus
+                autocomplete="name" :placeholder="__('Nombre Completo')" />
 
             <!-- Email Address -->
-            <flux:input
-                name="email"
-                :label="__('Email address')"
-                :value="old('email')"
-                type="email"
-                required
-                autocomplete="email"
-                placeholder="email@example.com"
-            />
+            <flux:input name="email" :label="__('Correo Electrónico')" :value="old('email')" type="email" required
+                autocomplete="email" placeholder="email@gmail.com" />
 
             <!-- Password -->
-            <flux:input
-                name="password"
-                :label="__('Password')"
-                type="password"
-                required
-                autocomplete="new-password"
-                :placeholder="__('Password')"
+            <flux:input name="password" :label="__('Contraseña')" type="password" required autocomplete="new-password"
+                :placeholder="__('Contraseña')"
                 passwordrules="{{ \Illuminate\Validation\Rules\Password::defaults()->toPasswordRulesString() }}"
-                viewable
-            />
+                viewable />
 
             <!-- Confirm Password -->
-            <flux:input
-                name="password_confirmation"
-                :label="__('Confirm password')"
-                type="password"
-                required
-                autocomplete="new-password"
-                :placeholder="__('Confirm password')"
+            <flux:input name="password_confirmation" :label="__('Confirmar Contraseña')" type="password" required
+                autocomplete="new-password" :placeholder="__('Confirmar Contraseña')"
                 passwordrules="{{ \Illuminate\Validation\Rules\Password::defaults()->toPasswordRulesString() }}"
-                viewable
-            />
+                viewable />
 
             <div class="flex items-center justify-end">
                 <flux:button type="submit" variant="primary" class="w-full" data-test="register-user-button">
-                    {{ __('Create account') }}
+                    {{ __('Crear cuenta') }}
                 </flux:button>
             </div>
         </form>
 
         <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
-            <span>{{ __('Already have an account?') }}</span>
-            <flux:link :href="route('login')" wire:navigate>{{ __('Log in') }}</flux:link>
+            <span>{{ __('¿Ya tienes una cuenta?') }}</span>
+            <flux:link :href="route('login')" wire:navigate>{{ __('Iniciar Sesión') }}</flux:link>
         </div>
     </div>
 </x-layouts::auth>
